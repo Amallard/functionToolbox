55392d1fc768ce60315ebd9177281721da56c489
Adam Allard <adam.allard@live.com>
2016-05-10 21:18:01 -0700

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,24 +1,28 @@
 void quickSort(vector<int> &arr, int left, int right) {
-	int i = left, j = right;
-	int tmp;
-	int pivot = arr[(left + right) / 2];
-
-	while (i <= j) {
-		while (arr[i] < pivot)
-			i++;
-		while (arr[j] > pivot)
-			j--;
-		if (i <= j) {
-			tmp = arr[i];
-			arr.at(i) = arr.at(j);
-			arr.at(j) = tmp;
-			i++;
-			j--;
-		}
+    int i = left, j = right;
+    int tmp;
+    int pivot = arr[(left + right) / 2];
+    
+    while (i <= j) {
+    	while (arr[i] < pivot) {
+    	    i++;
+    	}
+	while (arr[j] > pivot) {
+	    j--;
 	}
+	if (i <= j) {
+	    tmp = arr[i];
+	    arr.at(i) = arr.at(j);
+	    arr.at(j) = tmp;
+	    i++;
+	    j--;
+	}
+    }
 
-	if (left < j)
-		quickSort(arr, left, j);
-	if (i < right)
-		quickSort(arr, i, right);
+    if (left < j) {
+        quickSort(arr, left, j);
+    }
+    if (i < right) {
+    quickSort(arr, i, right);
+    }
 }
